5e29d02e869f7518b090d2c00f999d7a744daf1a
lsleena <lsleena@gmail.com>
2024-06-15 17:05:36 -0700

    Working schema

--- source/job/batch/jobs/schema_read.py
+++ source/job/batch/jobs/schema_read.py
@@ -29,14 +29,35 @@ schema = dfSchema.schema
 # Read new json files using this schema by using `.schema()`. Works on higher runtimes as well since spark now does not have to create schema on its own.
 intermediate_df = spark.read.option("multiline","true").schema(schema).json("/Users/leenapatil/Documents/all_json/1*.json")
 
-match_num_df = intermediate_df.withColumn("input_file_name",input_file_name())
-new_df = match_num_df.withColumn('input_file_name', f.regexp_extract(f.col('input_file_name'), '[0-9]+', 0))
+match_num_df1 = intermediate_df.withColumn("input_file_name",input_file_name())
 
-innings_df = new_df.withColumn("innings", explode(col("innings")))
+match = match_num_df1.withColumn("input_file_name", f.regexp_extract(f.col('input_file_name'), '[0-9]+', 0)) \
+    .withColumn("balls_per_over", col("info.balls_per_over")) \
+    .withColumn("dates", explode(col("info.dates"))) \
+    .withColumn("match_gender", col("info.gender")) \
+    .withColumn("match_type", col("info.match_type")) \
+    .withColumn("match_winner", col("info.outcome.winner")) \
+    .withColumn("match_season", col("info.season")) \
+    .withColumn("match_team_type", col("info.team_type")) \
+    .withColumn("team_1", col("info.teams").getItem(0)) \
+    .withColumn("team_1", col("info.teams").getItem(1)) \
+    .withColumn("player_of_match", explode("info.player_of_match")) \
+    .withColumn("toss_decision", col("info.toss.decision")) \
+    .withColumn("toss_winner", col("info.toss.winner")) \
+    .withColumn("match_venue", col("info.venue")) \
+    .withColumn("innings", explode(col("innings"))) \
+    .withColumn("overs", explode("innings.overs")) \
+    .withColumn("over_number", col("overs.over")) \
+    .withColumn("deliveries", explode("overs.deliveries")) \
+    .withColumn("batter", col("deliveries.batter")) \
+    .withColumn("bowler", col("deliveries.bowler")) \
+    .withColumn("non_striker", col("deliveries.non_striker")) \
+    .withColumn("run_scored", col("deliveries.runs.total")) \
+    .withColumn("wickets", explode("deliveries.wickets")) \
+    .withColumn("player_out", col("wickets.player_out")) \
+    .withColumn("fielders", explode("wickets.fielders")) \
+    .withColumn("fielders_name", col("fielders.name")) \
+    .withColumn("kind_out_by", col("wickets.kind")) \
+    .drop("info","innings", "deliveries", "overs", "wickets", "fielders","meta")
 
-overs_df = innings_df.withColumn("overs", explode("innings.overs"))
-
-deliveries_df = overs_df.withColumn("deliveries", explode("overs.deliveries"))
-data1_df = deliveries_df.select("innings.team","innings.declared","deliveries.batter","deliveries.bowler", "deliveries.runs.total","deliveries.wickets")
-
-#data1_df.coalesce(4).write.format("parquet").save("/Users/leenapatil/Documents/match_deliveries_fact_table/")
+match.show(truncate=False)
